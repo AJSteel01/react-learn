364d34a9cbec704f082870e5cb237ae212603fe7
AJSteel01 <abhigyan.209301092@muj.manipal.edu>
2024-03-06 13:06:29 +0530

    react basics practice and experimenting

---
+++ react-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const name = "Abhigyan"
+  return (
+    <>
+      <h1>React Revision and testing </h1>
+      <h2>By {name}</h2>
+    </>
+  )
+}
+
+export default App
